4667ab94d0847dde1c60217dfbf606da49d57fe7
Stephen Hadley <stephenh@cogability.com>
2022-09-16 07:58:19 -0500

    add some logging

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+	console.log('Hello World!')
+	res.send('Hello World!')
 })
 
 app.get('/env', (req, res) => {
-  res.send(process.env)
+	console.log('/env')
+	res.send(process.env)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+	console.log(`Example app listening on port ${port}`)
 })
